169683cf1cbc753a9ba36a2da35f668092cf5671
iFNokkkk <only.skytest633@gmail.com>
2022-02-19 09:29:51 +0700

    initial Project

---
+++ JavaBoot_Work-I/src/main/java/com/work1/JavaBoot_WorkI/JavaBootWorkIApplication.java
@@ -0,0 +1,13 @@
+package com.work1.JavaBoot_WorkI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootWorkIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootWorkIApplication.class, args);
+	}
+
+}
---
+++ JavaBoot_Work-I/src/test/java/com/work1/JavaBoot_WorkI/JavaBootWorkIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work1.JavaBoot_WorkI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBootWorkIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
